d1886bcbe0010c1f8e05b733dc2ecf2811a63e5c
Wahib <wahib1900@gmail.com>
2023-03-27 13:07:33 +0800

    Prototype demo

---
+++ Hubs/AppHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ServerApp_Prototype.Hubs
+{
+
+  public class AppHub : Hub
+  {
+    public Task BroadCastMessage(string sender, string message)
+    {
+      return Clients.Others.SendAsync(method: "ReceiveMessage", sender, message);
+    }
+  }
+}
